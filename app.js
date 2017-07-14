4cc11576f199ddb4b94377b9127bc28af891fd17
OstapHetman <prickly4528@gmail.com>
2017-07-14 14:37:30 +0300

    connect to database, checking connection

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to DATABASE
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database' + config.database)
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error' + err)
+});
 
 const app = express();
 
@@ -14,6 +27,9 @@ const port = 3000;
 
 // CORS Middleware
 app.use(cors());
+
+// Set static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
