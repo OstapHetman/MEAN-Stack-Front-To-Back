50d7e9331a63eeb76aa2f2ad709dad426b7a9efe
OstapHetman <prickly4528@gmail.com>
2017-07-15 23:51:21 +0300

    implement Login

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,3 +1,6 @@
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { AuthService } from './../../servises/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router:Router,
+    private flashMessage:FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit(){
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if(data.success){
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessage.show('You are now logged in', {
+          cssClass: 'alert-success',
+          timeout: 5000});
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessage.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeout: 5000});
+        this.router.navigate(['login']);
+      }
+    });
   }
 
 }
