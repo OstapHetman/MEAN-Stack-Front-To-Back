ab743c49911d44e57001692c943bf131484cfc29
OstapHetman <prickly4528@gmail.com>
2017-07-15 21:05:08 +0300

    implement routes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/src/app/components/navbar/navbar.component';
-import { LoginComponent } from './components/src/app/components/login/login.component';
-import { RegisterComponent } from './components/src/app/components/register/register.component';
-import { HomeComponent } from './components/src/app/components/home/home.component';
-import { DashboardComponent } from './components/src/app/components/dashboard/dashboard.component';
-import { ProfileComponent } from './components/src/app/components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +31,8 @@ import { ProfileComponent } from './components/src/app/components/profile/profil
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
